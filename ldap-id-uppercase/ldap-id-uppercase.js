ab24fc547291f5af54bbf548b7eae2caaf916079
Sven Seeberg <mail@sven-seeberg.de>
2024-03-20 14:54:05 +0100

    Use getAttributeStream

--- ldap-id-uppercase/ldap-id-uppercase.js
+++ ldap-id-uppercase/ldap-id-uppercase.js
@@ -1,2 +1,9 @@
-var ldap_id_uppercase = user.getAttribute("LDAP_ID").toUpperCase();
+var ldap_id_uppercase = "";
+if ( user && user.getAttributeStream("LDAP_ID") ) {
+  ldap_id_uppercase = user.getAttributeStream("LDAP_ID").toUpperCase();
+  print("LDAP_ID: " + ldap_id_uppercase)
+} else {
+  print("LDAP_ID attribute does not exist")
+}
+
 exports = ldap_id_uppercase;
