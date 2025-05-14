a98140da5af579f66783aeda08bb6b62dc3b2397
Sven Seeberg <mail@sven-seeberg.de>
2025-05-14 07:57:58 +0200

    Allow using lower case ldap ID

--- ldap-id-uppercase/ldap-id-uppercase.js
+++ ldap-id-uppercase/ldap-id-uppercase.js
@@ -1,6 +1,18 @@
-var ldap_id_uppercase = "";
-user.getAttributeStream("LDAP_ID").forEach(function(ldap_id) {
-  ldap_id_uppercase = ldap_id.toUpperCase();
-  print("Upper Case LDAP_ID: " + ldap_id_uppercase)
+var converted_ldap_id = "";
+var lower_case_ldap_id = false;
+
+user.getAttributeStream("lower_case_ldap_id").forEach(function(lower_case_ldap_id_attribute) {
+  if (lower_case_ldap_id_attribute == "true") {
+    lower_case_ldap_id = true;
+  }
 });
-exports = ldap_id_uppercase;
+
+user.getAttributeStream("LDAP_ID").forEach(function(ldap_id) {
+  if (lower_case_ldap_id) {
+    converted_ldap_id = ldap_id.toLowerCase();
+  } else {
+    converted_ldap_id = ldap_id.toUpperCase();
+  }
+  print("Upper Case LDAP_ID: " + converted_ldap_id)
+});
+exports = converted_ldap_id;
